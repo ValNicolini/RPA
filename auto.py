67c02a1dcefd99aed05d9a97cd3b9a3e9a5d65d6
ValNicolini <val_nicolini13@hotmail.com>
2023-09-21 16:13:26 -0300

    Empresa

--- auto.py
+++ auto.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 chrome_options = Options()
@@ -11,40 +14,32 @@ service = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=service, options=chrome_options)
 navegador.maximize_window()
 
-navegador.get('https://www.cursoemvideo.com/')
-
-navegador.find_element('xpath', '//*[@id="menu-item-42376"]/a/span').click()
-navegador.execute_script("window.scrollTo(0, Y)")
+# navegador.get('https://www.google.com.br/?hl=pt-BR')
+# navegador.find_element('xpath', '//*[@id="APjFqb"]').send_keys('cuso em video')
+# sleep(2)
+# navegador.find_element('xpath', '//*[@id="APjFqb"]').send_keys(Keys.ENTER)
+# sleep(2)
+# navegador.find_element('xpath', '//*[@id="rso"]/div[1]/div/div/div/table/tbody/tr[1]/td/div/h3/a').click()
+# navegador.find_element('xpath', '//*[@id="cn-accept-cookie"]').click()
 # navegador.find_element('xpath','//*[@id="post-42350"]/div/div[1]/div/div/div[2]/div/div[1]/div/div[3]/div/div/div/form/div[3]/input').send_keys('val_nicolini13@hotmail.com')
-# navegador.find_element('xpath', '//*[@id="uabb-password-field"]').send_keys('11q222q1')
-# navegador.find_element('xpath','//*[@id="cn-accept-cookie"]').click()
-# navegador.find_element('xpath','//*[@id="post-42350"]/div/div[1]/div/div/div[2]/div/div[1]/div/div[3]/div/div/div/form/div[7]/div/button/span').click()
+# navegador.find_element('xpath','//*[@id="uabb-password-field"]').send_keys('11q222q1')
+# navegador.find_element('xpath', '//*[@id="post-42350"]/div/div[1]/div/div/div[2]/div/div[1]/div/div[3]/div/div/div/form/div[7]/div/button').click()
+# sleep(2)
+# navegador.find_element('xpath', '//*[@id="menu-item-1508"]/a/span').click()
+navegador.get('https://outlook.office.com/mail/')
+WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('xpath', '//*[@id="i0116"]')))
+navegador.find_element('xpath',  '//*[@id="i0116"]').send_keys('silva.valdenir@hcosta.com.br')
+WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('xpath', '//*[@id="idSIButton9"]')))
+navegador.find_element('xpath', '//*[@id="idSIButton9"]').click()
+sleep(2)
+navegador.find_element('xpath', '//*[@id="rso"]/div[1]/div/div/div/table/tbody/tr[1]/td/div/h3/a').click()
+navegador.find_element('xpath', '//*[@id="cn-accept-cookie"]').click()
+navegador.find_element('xpath','//*[@id="post-42350"]/div/div[1]/div/div/div[2]/div/div[1]/div/div[3]/div/div/div/form/div[3]/input').send_keys('val_nicolini13@hotmail.com')
+navegador.find_element('xpath','//*[@id="uabb-password-field"]').send_keys('11q222q1')
+navegador.find_element('xpath', '//*[@id="post-42350"]/div/div[1]/div/div/div[2]/div/div[1]/div/div[3]/div/div/div/form/div[7]/div/button').click()
+sleep(2)
+navegador.find_element('xpath', '//*[@id="menu-item-1508"]/a/span').click()
 
-# navegador.find_element('xpath',
-#                        '//*[@id="docking_InitVisiblePart_0"]/div/div[3]/div[1]/div/div[4]/div/div/div[1]').send_keys('val_nicolini13@hotmail.com')
-# navegador.find_element('xpath',
-#                        '//*[@id="TextField762"]').send_keys('Testando o Robô')
-# navegador.find_element('xpath',
-#                        '//*[@id="sonoraIntroHintParent"]/span[1]').send_keys('Email de teste!'
-#                                                                              'Não responda.'
-#                                                                              ''
-#                                                                              'Atenciosamente...')
-# navegador.find_element('xpath',
-#                        '//*[@id="id__747"]').click()
-#
-# navegador.quit()
-=======
-navegador.get('https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M')
-navegador.find_element('xpath',
-                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys('Nicolini')
-navegador.find_element('xpath',
-                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys('val_nicolini13@hotmail.com')
-navegador.find_element('xpath',
-                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[3]/div/input').send_keys('14-998879246')
-navegador.find_element('xpath',
-                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/button/span/b').click()
-navegador.find_element('xpath',
-                       '//*[@id="popup-container"]/div/div/a[1]').click()
 sleep(7)
 navegador.quit()
->>>>>>> parent of a7b5fb7 (Update auto.py)
+
