2c3088d6bf4f7656dfc5f3dd53d6e3dbb14a1cac
ValNicolini <val_nicolini13@hotmail.com>
2023-09-21 13:31:52 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -10,6 +10,7 @@ chrome_options.add_experimental_option("detach", True)
 service = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=service, options=chrome_options)
 navegador.maximize_window()
+<<<<<<< HEAD
 navegador.get('https://www.cursoemvideo.com/')
 
 navegador.find_element('xpath', '//*[@id="menu-item-42376"]/a/span').click()
@@ -32,3 +33,18 @@ navegador.execute_script("window.scrollTo(0, Y)")
 #                        '//*[@id="id__747"]').click()
 #
 # navegador.quit()
+=======
+navegador.get('https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M')
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys('Nicolini')
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys('val_nicolini13@hotmail.com')
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[3]/div/input').send_keys('14-998879246')
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/button/span/b').click()
+navegador.find_element('xpath',
+                       '//*[@id="popup-container"]/div/div/a[1]').click()
+sleep(7)
+navegador.quit()
+>>>>>>> parent of a7b5fb7 (Update auto.py)
