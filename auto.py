a144496528ab76a082376e5657eca92e9adb88ed
ValNicolini <val_nicolini13@hotmail.com>
2023-10-24 12:02:22 -0300

    Empresa

--- auto.py
+++ auto.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 # from webdriver_manager.chrome import ChromeDriverManager
 # from selenium.webdriver.chrome.options import Options
 # from selenium.webdriver.chrome.service import Service
@@ -17,27 +18,27 @@ from datetime import date
 # opcoes.add_argument("--headless=new")
 
 navegador = webdriver.Firefox()
-navegador.get('https://www.hashtagtreinamentos.com/cursos-hashtag-programacao?')
+navegador.get('https://www.hashtagtreinamentos.com/curso-python')
 #navegador.maximize_window()
 
 
 def teste(x):
      #espera satisfazer as condições
-     return WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('xpath', x)))
+     return WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('css selector', x)))
 
 
 def busca(y):
      return navegador.find_element('css selector', y)
 
 
-busca('.wp-image-13719 > img:nth-child(2)').click()
-# sleep(1)
-# busca('div.bloco-textoinput-formulario-excel-empe:nth-child(8) > input:nth-child(2)').send_keys('Val Nicolini')
-
-
 lista = navegador.find_elements(By.ID, 'firstname')
-sleep(5)
-print(len(lista))
+email = 
+# sleep(2)
+# print(len(lista))
+
+busca((lista[1]).send_keys('Val Nicolini'))
+busca(lista[1]).send_keys(Keys.ENTER)
+busca(lista[1]).send_keys('val_nicolini13@hotmail.com')
 
 
 
