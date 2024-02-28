42dc6339a7563e73ccd5611cee7f5e444572c6d0
ValNicolini <val_nicolini13@hotmail.com>
2024-02-28 15:47:34 -0300

    Update PlayWright.py

--- PlayWright.py
+++ PlayWright.py
@@ -2,8 +2,28 @@ from playwright.sync_api import sync_playwright
 from time import sleep
 
 with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto('https://files-admin.hcosta.com.br/login')
-    print(page.screenshot(path='Teste.png'))
-    browser.close()
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        # Navegar até a página com o formulário
+        page.goto('https://files-admin.hcosta.com.br/login')
+
+        # Preencher o formulário
+        page.fill("input[name='accessKey']", 'A279BJ9KMB6BZ2U5')
+        page.fill("input[name='secretKey']", 'K!))9ZCU,!Y}L@,.2)!]UW6}3BZ.}TX-')
+        # Submeter o formulário
+        page.click("button[type= 'submit']")
+        page.locator("path[d='M244.1,8.4c-3.9-5.3-10.1-8.5-16.7-8.5H21.6C15,0,8.8,3.1,4.9,8.4C0.8,14-0.9,21,0.3,27.9 c5.1,29.6,15.8,91.9,24.3,141.7v0.1C29,195,32.8,217.1,35,229.9c1.4,10.8,10.4,18.9,21.3,19.3h136.5 c10.9-0.4,19.9-8.5,21.3-19.3l10.3-60.1l0.1-0.4L238.4,88v-0.2l10.3-59.9C249.9,21,248.3,14,244.1,8.4 M206.1,177h-163 l-3.2-18.6h169.3L206.1,177z M220,95.3H28.9l-3.2-18.6h197.4L220,95.3z']")
+        page.locator('g-d >> nth=1').click()
+        page.wait_for_timeout(5000)
+
+
+        # Esperar pela navegação para verificar se o formulário foi submetido com sucesso
+
+
+
+
+        browser.close()
+
+
+
