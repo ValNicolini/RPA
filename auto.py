86a1eebafc85260b422daf4d2b7280e1eaaa810e
ValNicolini <val_nicolini13@hotmail.com>
2024-03-11 11:06:54 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,14 +1,11 @@
 
 from playwright.sync_api import sync_playwright
 
-from  require import require
 
-chromium = require('playwright')
-expect = require('expect')
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=50)
+    browser = p.chromium.launch(headless=False)
     page = browser.new_page()
 
     # Navegar até a página com o formulário
-    page.goto('http://todomvc.com/examples/react/#/')
+    page.goto('https://todomvc.com/')
