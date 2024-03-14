6744e48e09799d3cab386ee63ba57cd8f2b432a2
ValNicolini <val_nicolini13@hotmail.com>
2024-03-14 15:24:56 -0300

    Quinta

--- Extrair_Relatorio.py
+++ Extrair_Relatorio.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 from time import sleep
 from datetime import date
 data = date.today()
@@ -14,16 +14,10 @@ with sync_playwright() as p:
         page.fill("input[name= 'senha']", 'Nico#1307')
         page.click("button[type= 'submit']")
         sleep(3)
-        # page.click('#opcoes > ul > li > span')
-        # page.click("xpath=//div[@id='opcoes']/ul/li/span")
-        # page.click("ul >> li >> xpath= '/html/body/div[6]/ul[5]/li/ul/li[1]/span'")
-        # page.click("span[id='m_5_8_8_0']")
+        teste = page.locator('.opcoes')
 
-        # page.locator("xpath= /html/body/div[6]/ul[5]/li/ul/li[2]/span").click()
-        # page.locator('xpath= //*[@id="object-list-wrapper"]/div/div[2]/ul/li[5]/span/button/span[2]').click()
-        # page.click("button[type= 'submit']")
-        # page.click("a[href='/buckets/arquivos-importacao/browse']")
-        sleep(3)
+
+
 
 
 
--- PlayWright.py
+++ PlayWright.py
@@ -9,7 +9,7 @@ br = data.strftime('%d/%m/%Y')
 hora = data.strftime('%H:%M')
 
 with sync_playwright() as p:
-        browser = p.chromium.launch() #args=["--start-maximised"],
+        browser = p.chromium.launch(headless=False) #args=["--start-maximised"],
         page = browser.new_page() #no_viewport=True
 
 
@@ -18,22 +18,14 @@ with sync_playwright() as p:
         page.goto('https://files-admin.hcosta.com.br/login')
 
         # Preencher o formulário
-        page.fill("input[name='accessKey']", 'A279BJ9KMB6BZ2U5')
-        page.fill("input[name='secretKey']", 'K!))9ZCU,!Y}L@,.2)!]UW6}3BZ.}TX-')
+        page.locator("#accessKey").fill('A279BJ9KMB6BZ2U5')
+        page.locator("#secretKey").fill('K!))9ZCU,!Y}L@,.2)!]UW6}3BZ.}TX-')
         # Submeter o formulário
-        page.click("button[type= 'submit']")
+        page.click("#do-login")
         page.click("a[href='/buckets/arquivos-importacao/browse']")
 
         page.wait_for_timeout(6000)
 
-        # x = 20
-        # for i in range(x):
-        #    sleep(30)
-        #    # Encontrar todos os checkboxes a partir do segundo
-        #    checkboxes = page.query_selector_all('input[type="checkbox"]')[1:11]
-
-        # inicio = ("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[1]/div[1]/span/input")
-        # Iterar sobre os checkboxes e marcá-los
         x = 1
         for t in range(x):
           y = 10
@@ -41,28 +33,11 @@ with sync_playwright() as p:
                 i+=1
                 page.click(f"xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[{i}]/div[1]/span/input")
 
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[2]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[3]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[4]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[5]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[6]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[7]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[8]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[9]/div[1]/span/input")
-                # page.click("xpath= //*[@id='object-list-wrapper']/div/div[1]/div[2]/span/div/div[1]/div/div[2]/div/div[10]/div[1]/span/input")
-
-
-            # page.keyboard.press('ArrowDown')
-            # sleep(0.5)
-
-           # page.keyboard.press('ArrowDown')
           page.wait_for_timeout(2000)
           page.locator('xpath= //*[@id="object-list-wrapper"]/div/div[2]/ul/li[5]/span/button/span[2]').click()
           page.wait_for_timeout(2000)
           page.click("button[label='Delete']")
           page.wait_for_timeout(7000)
-
-
 
         browser.close()
         print(f'Hoje {br}\nÁs {hora}\nForam excluídos: {x*y} arquivos!')
