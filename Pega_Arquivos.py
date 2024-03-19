a806f56d603d62e71852a1c098f1aea3a8f58d57
ValNicolini <val_nicolini13@hotmail.com>
2024-03-19 15:30:52 -0300

    Update Pega_Arquivos.py

--- Pega_Arquivos.py
+++ Pega_Arquivos.py
@@ -26,23 +26,24 @@ def listar_arquivos_por_tipo(diretorio, tipo_arquivo):
 def main():
     # Diretório da pasta onde os arquivos estão localizados
     diretorio = r'\\172.25.5.25\rvs\inbox'
-
-    # Tipo de arquivo desejado (por exemplo, '.txt', '.jpg', '.pdf')
     tipo_arquivo = f'.{data_atual}0907520000'
 
     # Listar arquivos do tipo especificado na pasta
-    arquivos_tipo_especificado = listar_arquivos_por_tipo(diretorio, tipo_arquivo)
+    while True:
+           arquivos_tipo_especificado = listar_arquivos_por_tipo(diretorio, tipo_arquivo)
 
-    # Imprimir os arquivos encontrados
-    if arquivos_tipo_especificado:
-        alerta = Notification(app_id='Volks', title='ATENÇÃO!', msg='Arquivo Volks chegou!', duration='long',
-                              icon=r'C:\xampp\htdocs\GitHub\RPA\images.jpg')
-        alerta.set_audio(audio.LoopingAlarm, loop=False)
-        alerta.add_actions(label="Volks",launch=r"\\172.25.5.25\rvs\inbox")
-        alerta.show()
+           if arquivos_tipo_especificado:
+               alerta = Notification(app_id='Volks', title='ATENÇÃO!', msg='Arquivo Volks chegou!', duration='long',
+                                     icon=r'C:\xampp\htdocs\GitHub\RPA\images.jpg')
+               alerta.set_audio(audio.LoopingAlarm, loop=False)
+               alerta.add_actions(label="Volks", launch=r"\\172.25.5.25\rvs\inbox")
+               alerta.show()
+               break
+           else:
+               return
 
-    else:
-        print("Nenhum arquivo encontrado na pasta.")
+
+
 
 if __name__ == "__main__":
-    main()
+     main()
