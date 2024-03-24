f2727cdf4db8d7850b663387dab42daedccd4722
ValNicolini <val_nicolini13@hotmail.com>
2024-03-24 18:53:57 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 from datetime import datetime
 
 servico = Service(GeckoDriverManager().install())
 navegador = webdriver.Firefox(service=servico)
 
-navegador.get("http://sistema.hcosta.com.br/hcosta/index.php")
+data_atual = datetime.now()
+data_formatada = data_atual.strftime("%d/%m/%Y")
+
+navegador.get("https://sistema.hcosta.com.br/hcosta/")
 navegador.find_element('xpath', '//*[@id="nome"]').send_keys("silva.valdenir")
 navegador.find_element('xpath', '//*[@id="senha"]').send_keys("Nico#1307")
 navegador.find_element('xpath', '//*[@id="botao_aceitar"]').click()
@@ -21,12 +23,8 @@ sleep(1)
 navegador.find_element('xpath', '//*[@id="m_5_8_8_0"]').click()
 sleep(3)
 navegador.switch_to.frame('conteudo')
-data_atual = datetime.now()
-data_formatada = data_atual.strftime("%d/%m/%Y")
-campo_data_inicio = navegador.find_element('xpath', '//*[@id="data_inicial"]')
-navegador.execute_script("arguments[0].value = arguments[1];", campo_data_inicio, data_formatada)
-campo_data_fim = navegador.find_element('xpath', '//*[@id="data_final"]')
-navegador.execute_script("arguments[0].value = arguments[1];", campo_data_fim, data_formatada)
+navegador.find_element('xpath', '//*[@id="data_inicial"]').send_keys(data_formatada)
+navegador.find_element('xpath', '//*[@id="data_final"]').send_keys(data_formatada)
 navegador.find_element('xpath', '/html/body/form/fieldset/ul/li[11]/button').click()
-
-sleep(4)
+alerta = navegador.switch_to.alert
+alerta.accept()
