0465cebad1cd915665675b50eb2a5b11bcbd6122
ValNicolini <val_nicolini13@hotmail.com>
2024-04-20 20:59:43 -0300

    Update Testes.py

--- Testes.py
+++ Testes.py
@@ -1,20 +1,39 @@
 
 
-def teste():
-    print('Teste')
-def nome():
-    print('Val')
 
-def numero():
-    print(4)
+opcao = int(input('Digite um número: '))
+
+match opcao:
+    case 1:
+        print('Digitou 1')
+    case 2:
+        print('Digitou 2')
+    case 3:
+        print('Digitou 3')
+    case 4:
+        print('Digitou 4')
+    case _:
+        print('Sair')
 
 
-def main():
-    numero()
-    nome()
-    teste()
+
+# def teste():
+#     print('1')
+# def nome():
+#     print('2')
+#
+# def numero():
+#     print('3')
+#
+# def outro():
+#     print('4')
+#
+# def main():
+#     numero()
+#     nome()
+#     teste()
+#
 
 
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
