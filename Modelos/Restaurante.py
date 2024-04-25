c507f99e9b26ea42f489310e13afeb1363f0173d
ValNicolini <val_nicolini13@hotmail.com>
2024-04-25 17:38:58 -0300

    Update Restaurante.py

--- Modelos/Restaurante.py
+++ Modelos/Restaurante.py
@@ -2,22 +2,29 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
 
     @classmethod
-
     def listar_restaurantes(cls):
+        print(f"{'Nome'.ljust(20)} | {'Categoria'.ljust(20)} | {'Status'.ljust(20)}")
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo.ljust(20)}')
 
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Inativo'
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
+    def alternar_status(self):
+        self._ativo = not self._ativo
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
+restaurante_pizza.alternar_status()
 Restaurante.listar_restaurantes()
