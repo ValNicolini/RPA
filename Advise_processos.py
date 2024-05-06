a92888e56459ccb6c0d1f7165a202b56544fc1df
ValNicolini <val_nicolini13@hotmail.com>
2024-05-06 15:16:33 -0300

    Feito

--- Advise_processos.py
+++ Advise_processos.py
@@ -17,7 +17,7 @@ def capturar_nprocesso(xml_path):
     return resultados
 
 # Exemplo de uso
-caminho_do_xml = r'C:\xampp\htdocs\GitHub\Extrator_url\ADVOCACIAHCOSTA - (120)_8_.xml'
+caminho_do_xml = r'C:\xampp\htdocs\GitHub\RPA\ADVOCACIAHCOSTA - (250)_3.xml'
 resultados = capturar_nprocesso(caminho_do_xml)
 
 # Imprime os resultados capturados
